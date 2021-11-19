339cfd0767791e18ba1eda55e3d21f94fec56ac4
pzjyyzs <463669395@qq.com>
2021-11-20 06:33:07 +0800

    添加状态

--- promise/demo.js
+++ promise/demo.js
@@ -1,19 +1,36 @@
+// 先定义三个常量表示状态
+const PENDING = 'pending';
+const FULFILLED = 'fulfilled';
+const REJECTED = 'rejected';
+
 class MyPromise {
     constructor(excutor) {
-        console.log(excutor)
         excutor(this.resolve, this.reject)
     }
 
-    resolve = () => {  console.log(this) }
-    reject = () => {}
-   /*  resolve() {
-        console.log(this)
-    }
-    reject() {
+    status = PENDING;
+    value = null;
+    reason = null;
 
-    } */
+    resolve = (value) => {  
+        // 只有在pending下才更改状态 
+        if (this.status === PENDING) {
+            this.status = FULFILLED;
+
+            this.value = value;
+        }
+    }
+
+    reject = (reason) => {
+         // 只有在pending下才更改状态 
+        if (this.status === PENDING) {
+            this.status = REJECTED;
+
+            this.reason = reason;
+        }
+    }
 }
 
-let a = new MyPromise((a, b) => {
-    a()
+let a = new MyPromise((res, rej) => {
+    res()
 })
