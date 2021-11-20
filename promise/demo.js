12744a7ebfd1c269ca0cb81406c40775c965d464
pzjyyzs <463669395@qq.com>
2021-11-20 08:48:05 +0800

    简单处理异步问题

--- promise/demo.js
+++ promise/demo.js
@@ -12,12 +12,20 @@ class MyPromise {
     value = null;
     reason = null;
 
+    // 存储成功回调函数
+    onFulfilledCallback = null;
+    // 存储失败回调函数
+    onRejectedCallback = null;
+
+
     resolve = (value) => {  
         // 只有在pending下才更改状态 
         if (this.status === PENDING) {
             this.status = FULFILLED;
 
             this.value = value;
+
+            this.onFulfilledCallback && this.onFulfilledCallback(value);
         }
     }
 
@@ -27,6 +35,8 @@ class MyPromise {
             this.status = REJECTED;
 
             this.reason = reason;
+
+            this.onRejectedCallback && this.onRejectedCallback(reason);
         }
     }
 
@@ -35,14 +45,19 @@ class MyPromise {
             onFulfilled(this.value)
         } else if (this.status === REJECTED) {
             onRejected(this.reason)
+        } else if (this.status === PENDING) {
+            this.onFulfilledCallback = onFulfilled;
+            this.onRejectedCallback = onRejected;
         }
     }
 
 }
 
 let a = new MyPromise((res, rej) => {
-    //res('success');
-    rej('fail')
+    setTimeout(() => {
+        res('success');
+    }, 300)
+   /*  rej('fail') */
 })
 
 a.then(value => {
