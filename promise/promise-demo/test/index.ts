dc12bf2d097b8ded2b65404dddc07732df63784d
pzjyyzs <463669395@qq.com>
2023-10-08 00:52:42 +0800

    手写Promise 准备工作 单元测试环境安装

---
+++ promise/promise-demo/test/index.ts
@@ -0,0 +1,16 @@
+import * as chai from 'chai';
+import 'mocha';
+
+const assert: any = chai.assert;
+
+/* describe('Promise', () => {
+    it('是一个类', () => {
+
+    })
+}) */
+
+describe('Chai 的使用', () => {
+    it('可以测试相等', () => {
+        assert(1 === 1);
+    })
+})
